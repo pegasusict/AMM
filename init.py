7378bf473eeb61414825712624e4fc72999a1fe2
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-09-09 19:39:00 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3 -tt
-################################################################################
-## Audiophiles Music Manager                                 VER0.0.0PREALPHA ##
-## (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com ##
-## init.py                           AMM initializer         VER0.0.0PREALPHA ##
-## License: GPL v3                         Please keep my name in the credits ##
-################################################################################
-
+"""
+********************************************************************************
+** Audiophiles Music Manager                                 VER0.0.0PREALPHA **
+** (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com **
+** init.py                           AMM initializer         VER0.0.0PREALPHA **
+** License: MIT                            Please keep my name in the credits **
+********************************************************************************
+"""
 import fsop
 
 def load_conf(cfg_file='amm.cfg'):
@@ -18,22 +19,21 @@ def load_conf(cfg_file='amm.cfg'):
 
 def get_conf():
     global amm_config
-    source_dir=input('Please enter the source directory: ') # ask for source dir
-    while not fsop.verify_dir_exists(source_dir):                   # if the source dir does not exist
-            source_dir=input('Please enter a valid path: ') # ask again until we get a valid answer
+    source_dir = input('Please enter the source directory: ') # ask for source dir
+    while not fsop.verify_dir_exists(source_dir):             # if the source dir does not exist
+            source_dir = input('Please enter a valid path: ') # ask again until we get a valid answer
     amm_config{'source_dir'} = source_dir
-    target_dir=input('Please enter the target directory: ')
-    while not(fsop.verify_dir_exists(target_dir)):
-        must_create_dir=input('The target directory does not exist. Should I create it? (y/n)')
-        if must_create_dir=='y':
+    target_dir = input('Please enter the target directory: ')
+    while not(fsop.verify_dir_exists(target_dir)):            # needs rewrite
+        must_create_dir = input('The target directory does not exist. Should I create it? (y/n)')
+        if must_create_dir == 'y':
             fsop.create_dir(target_dir)
         else
-            target_dir=input('Please enter a different target directory.')
-    amm_config{'target_dir'} = target_dir
+            target_dir = input('Please enter a different target directory.')
+    amm_config('target_dir') = target_dir
     return result
 
 __main__():
     if not load_conf()
         if not get_conf()
-            exit('error loading or getting config')
-
+            exit('fatal config error')
