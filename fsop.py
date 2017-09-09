54ab5f500bda6f7bfb1fc63c20f4f6352a1790e3
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-09-09 19:29:03 +0200

    Update fsop.py

--- fsop.py
+++ fsop.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python3 -tt
-################################################################################
-## Audiophiles Music Manager                                 VER0.0.0PREALPHA ##
-## (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com ##
-## fsop.py                     Filesystem Operations         VER0.0.0PREALPHA ##
-## License: GPL v3                         Please keep my name in the credits ##
-################################################################################
-
+"""
+********************************************************************************
+** Audiophiles Music Manager                                 VER0.0.0PREALPHA **
+** (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com **
+** fsop.py                     Filesystem Operations         VER0.0.0PREALPHA **
+** License: GPL v3                         Please keep my name in the credits **
+********************************************************************************
+"""
 import os
 
 def verify_dir_exists(path):
-    result=os.path.isdir(path)
+    result = os.path.isdir(path)
     return result
 
 def read_src(src_dir):
     # store results in 3 lists/tuples/dicts
     # dirs, audiofiles, garbage
 
-
-
-
 def verify_file_exists(path):
-    result=os.path.isfile(path)
+    result = os.path.isfile(path)
     return result
 
 def delete_file(file_to_be_deleted):
     os.unlink(file_to_be_deleted)
     return True
 
-
-
+################################################################################
 fp = open(fname,'r')        # open for reading (must exist)
 fp = open(fname,'w')        # creates new file for writing
 fp = open(fname,'a')        # opens file for appending
-
-
