85aa909849f17f69adc005bdeea721189cbf1035
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-09-09 19:42:38 +0200

    Update amm.py

--- amm.py
+++ amm.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3 -tt
-################################################################################
-## Audiophiles Music Manager                                 VER0.0.0PREALPHA ##
-## (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com ##
-## amm.py                            main script             VER0.0.0PREALPHA ##
-## License: GPL v3                         Please keep my name in the credits ##
-################################################################################
-
+"""
+********************************************************************************
+** Audiophiles Music Manager                                 VER0.0.0PREALPHA **
+** (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com **
+** amm.py                            main script             VER0.0.0PREALPHA **
+** License: GPL v3                         Please keep my name in the credits **
+********************************************************************************
+"""
 # import libs
-import sys
+import sys.argv as argv
 
 # main function
 def main():
-    args = sys.argv
+    args = argv
     # parse arguments
     
 # standard boilerplate
