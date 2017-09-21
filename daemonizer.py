a92b9c12ce0aff58583a799418bdc75bb4711758
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-09-21 11:04:16 +0200

    1st sketch involving multithreading

---
+++ daemonizer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3 -tt
+"""
+************************************************************************
+** Audiophiles Music Manager                         VER0.0.0PREALPHA **
+** (C)2017 Mattijs Snepvangers                  pegasus.ict@gmail.com **
+** daemonizer.py              daemonizer             VER0.0.0PREALPHA **
+** License: MIT                    Please keep my name in the credits **
+************************************************************************
+"""
+# import libs
+
+def startDaemons() :
+    """Deamonizer start
+
+    Daemons needed:
+    FSOP/AFOPS => indexer/tagParser/dupsPurger/fingerprintGenerator/transcoder
+    qualityCalc
+    InetC => mbClient, albumArtFetcher, lyricsFetcher
+
+    """
+    if configLoaded == True
+    ### prepare daemons to be run
+
